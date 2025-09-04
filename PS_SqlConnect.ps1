c9237cba3268e0770095e11a79bc2809c65b1bdd
fred720 <47834537+fred720@users.noreply.github.com>
2025-09-04 08:44:52 -0500

    Add files via upload

---
+++ PS_SqlConnect.ps1
@@ -0,0 +1,12 @@
+﻿$query = "
+    SELECT d.[Name],r.[SP_MajorRevisionNumber]
+    FROM [TEST_PLANTpid].[T_Drawing] AS d
+    FULL JOIN [TEST_PLANTpid].[T_Revision] AS r
+    ON d.SP_ID = r.SP_DrawingID
+    order by Name"
+
+$instance = "ogc-pid-sql-01"
+$db = "TEST_PLANT"
+
+
+Invoke-Sqlcmd -query $query -ServerInstance $instance -Database $db
